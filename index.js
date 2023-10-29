cb2af7172a0a7032ee99660950a66b730c0499db
Genimax <urdnot.wrexx21@gmail.com>
2023-10-29 13:50:25 +0300

    test hosting

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const PORT = 80;
 app.use(express.json());
 app.use(cors());
 
+const audioFilesDirectory = path.join(__dirname, "audio");
+const videoFilesDirectory = path.join(__dirname, "videos");
+const imageFilesDirectory = path.join(__dirname, "images");
+
 /////////////////// Получить список объектов
 app.get("/data", (req, res) => {
   res.json(data);
@@ -28,22 +32,30 @@ app.get("/data/:id", (req, res) => {
 });
 
 ////////////////// Получить картинку по пути файла
-app.get("/images/:imageName", (req, res) => {
+app.get("/imageLink/:imageName", (req, res) => {
   const imageName = req.params.imageName;
-  res.sendFile(path.join(__dirname, "images", imageName));
+  const fileURL = `http://${req.headers.host}/image/${imageName}`;
+  res.json({ url: fileURL });
 });
+app.use("/image", express.static(imageFilesDirectory));
 
 ////////////////// Получить аудио по пути файла
-app.get("/audio/:audioName", (req, res) => {
+app.get("/audioLink/:audioName", (req, res) => {
   const audioName = req.params.audioName;
-  res.sendFile(path.join(__dirname, "audio", audioName));
+  const fileURL = `http://${req.headers.host}/audio/${audioName}`;
+  // res.sendFile(path.join(__dirname, "audio", audioName));
+  res.json({ url: fileURL });
 });
+app.use("/audio", express.static(audioFilesDirectory));
 
 ////////////////// Получить видео по пути файла
-app.get("/videos/:videoName", (req, res) => {
+app.get("/videoLink/:videoName", (req, res) => {
   const videoName = req.params.videoName;
-  res.sendFile(path.join(__dirname, "videos", videoName));
+  const fileURL = `http://${req.headers.host}/video/${videoName}`;
+  // res.sendFile(path.join(__dirname, "videos", videoName));
+  res.json({ url: fileURL });
 });
+app.use("/video", express.static(videoFilesDirectory));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
